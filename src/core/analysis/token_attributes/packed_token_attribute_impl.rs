ca2848803ab7e8481133f29738c434ba982e402c
Lu Xugang <luxugang@apache.org>
2025-09-11 00:49:59 +0800

    Implement StopFilter

--- src/core/analysis/token_attributes/packed_token_attribute_impl.rs
+++ src/core/analysis/token_attributes/packed_token_attribute_impl.rs
@@ -239,7 +239,7 @@ impl AttributeImpl for PackedTokenAttributeImpl {
 
     fn copy_to(&mut self, to: &mut Self::AttributeImpl) {
         let len = self.base.length();
-        let buf = self.base.buffer();
+        let buf = self.base.buffer_mut();
         to.base.copy_buffer(buf, 0, len);
         to.position_increment = self.position_increment;
         to.position_length = self.position_length;
