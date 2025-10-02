73167459264ce92789bc24f45d5ad016fe57ad94
Lu Xugang <luxugang@apache.org>
2025-10-02 17:24:09 +0800

    Add Some Comparator

--- src/core/search/top_docs.rs
+++ src/core/search/top_docs.rs
@@ -16,6 +16,8 @@
  */
 use crate::core::search::score_doc::ScoreDoc;
 use crate::core::search::total_hits::TotalHits;
+use crate::core::util::error::lucene_error::Result;
+use crate::core::util::{Comparator, ToInt};
 
 /// Represents hits returned.
 #[derive(Debug, Clone, PartialEq)]
@@ -33,6 +35,42 @@ impl TopDocs {
         Self {
             total_hits,
             score_docs,
+        }
+    }
+}
+
+struct ShardIndexTieBreaker;
+impl Comparator<ScoreDoc> for ShardIndexTieBreaker {
+    const TYPE: &'static str = "ShardIndexTieBreaker";
+
+    fn compare(&self, a: &ScoreDoc, b: &ScoreDoc) -> Result<i32> {
+        Ok(a.shard_index.cmp(&b.shard_index).to_int())
+    }
+}
+
+struct DocIdTieBreaker;
+impl Comparator<ScoreDoc> for DocIdTieBreaker {
+    const TYPE: &'static str = "DocIdTieBreaker";
+
+    fn compare(&self, a: &ScoreDoc, b: &ScoreDoc) -> Result<i32> {
+        Ok(a.doc.cmp(&b.doc).to_int())
+    }
+}
+
+struct DefaultTieBreaker {
+    shard_cmp: ShardIndexTieBreaker,
+    doc_cmp: DocIdTieBreaker,
+}
+
+impl Comparator<ScoreDoc> for DefaultTieBreaker {
+    const TYPE: &'static str = "DefaultTieBreaker";
+
+    fn compare(&self, a: &ScoreDoc, b: &ScoreDoc) -> Result<i32> {
+        let res = self.shard_cmp.compare(a, b)?;
+        if res != 0 {
+            Ok(res)
+        } else {
+            self.doc_cmp.compare(a, b)
         }
     }
 }
